abf0ab2a016178f9b2954e51e9098af433fe49c6
lucasmt <lucasmt@rice.edu>
2019-08-13 02:54:16 -0500

    Changes to main file

--- src/synthesis/main.cpp
+++ src/synthesis/main.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
 #include <memory>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 #include "cuddObj.hh"
 
 #include "BucketElimination.hpp"
+#include "ClusterAndJoin.hpp"
+#include "GreedyScan.hpp"
+#include "InOrderScan.hpp"
+#include "TailJoin.hpp"
 
 #include "optional.h"
 #include "DFAGameSolver.h"
 #include "DFALoader.h"
+#include "JoinAlgFactory.h"
+#include "Logger.h"
 #include "SyftMgr.h"
 #include "SymbolicDFA.h"
 #include "SymbolicDFAConverter.h"
@@ -21,65 +28,32 @@ using std::move;
 using std::cout;
 using std::endl;
 using std::unique_ptr;
+using std::unordered_map;
 using std::vector;
-
-/*
-BDDDict construct_vars(Cudd& cudd_mgr,
-		       const VarPartition& var_partition,
-		       const StateMap& state_map)
-{
-  vector<jet::Attr> ordering;
-  
-  jet::AttrSet state_vars = state_map.state_vars();
-  jet::AttrSet next_state_vars = state_map.next_state_vars();
-  
-  auto it1 = state_vars.begin(), it2 = next_state_vars.begin();
-  
-  while (it1 != state_vars.end()) {
-    ordering.push_back(*it1);
-    ordering.push_back(*it2);
-    ++it1;
-    ++it2;
-  }
-  
-  jet::AttrSet env_vars = var_partition.env_vars();
-  jet::AttrSet sys_vars = var_partition.sys_vars();
-
-  ordering.insert(ordering.end(), env_vars.begin(), env_vars.end());
-  ordering.insert(ordering.end(), sys_vars.begin(), sys_vars.end());
-
-  return BDDDict(cudd_mgr, jet::AttrRanking(ordering));
-}
-*/
-
-jet::AttrRanking compute_bucket_ranking(const vector<SymbolicDFA>& dfas,
-                                        const SyftMgr& mgr)
-{
-  jet::AttrSet vertices = mgr.output_vars();
-
-  vector<jet::AttrSet> hyperedges;
-  hyperedges.reserve(dfas.size());
-  
-  for (const SymbolicDFA& dfa : dfas)
-  {
-    hyperedges.push_back(dfa.output_vars());
-  }
-
-  return jet::AttrRanking::MCS(vertices, hyperedges);
-}
 
 int main(int argc, char ** argv){
 
-  if(argc != 4){
-      cout << "Usage: ./syft "
-	   << "<base-filename> <number-of-files> <partition-file>"
-	   << endl;
+  if(argc != 7){
+    cout << "Usage: " << argv[0]
+         << " <base-filename>"
+         << " <number-of-files>"
+         << " <partition-file>"
+         << " <join-algorithm>"
+         << " <var-ranking>"
+         << " <logging-option>"
+         << endl;
       return 0;
     }
 
     string base_filename = argv[1];
     size_t k = atoi(argv[2]);
     string partition_file = argv[3];
+    unordered_map<string, string> options;
+    options["alg"] = argv[4];
+    options["rank"] = argv[5];
+    string logging_option = argv[6];
+
+    unique_ptr<Logger> logger = Logger::build(logging_option);
 
     VarPartition var_partition = VarPartition::load(partition_file);
 
@@ -92,8 +66,12 @@ int main(int argc, char ** argv){
       dfas.push_back(dfa);
     }
     
+    logger->record_dfas(dfas);
+
     SyftMgr mgr(dfas, move(var_partition));
     
+    logger->record_vars(mgr);
+
     vector<SymbolicDFA> symbolic_dfas;
     symbolic_dfas.reserve(k);
 
@@ -102,16 +80,21 @@ int main(int argc, char ** argv){
       symbolic_dfas.push_back(SymbolicDFA(dfas[i], mgr));
     }
 
-    jet::AttrRanking bucket_ranking =
-      compute_bucket_ranking(symbolic_dfas, mgr);
-    
-    unique_ptr<jet::JoinAlgorithm> join_algorithm =
-      make_unique<jet::BucketElimination>(bucket_ranking);
+    logger->record_symbolic_dfas(symbolic_dfas, mgr);
 
+    JoinAlgFactory factory(options);
+    unique_ptr<jet::JoinAlgorithm> join_algorithm =
+      factory.build(symbolic_dfas, mgr);
     FactoredSynthesizer synthesizer(mgr.bdd_dict, move(join_algorithm));
     DFAGameSolver solver(move(mgr), move(synthesizer));
 
-    bool realizable = solver.realizability(symbolic_dfas);
+    logger->start_timer();
+
+    bool realizable = solver.realizability(symbolic_dfas, logger);
+
+    logger->record_elapsed_time();
+
+    logger->record_result(realizable);
 
     return 0;
 }
